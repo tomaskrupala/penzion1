89e991fd603bf098aa170a1a731fb00d79b3b960
Tomas Krupala <tk2006@seznam.cz>
2025-01-11 21:34:27 +0100

    5544

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,18 @@
 export default async function (eleventyConfig) {
-
-  
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("css");
- }  
-    
+   
+    eleventyConfig.setInputDirectory("./src");
+    eleventyConfig.setTemplateFormats("html,njk");
+    eleventyConfig.setIncludesDirectory("./templates");
+ 
+    eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/css");
+ 
+    return {
+     
+      markdownTemplateEngine: "njk",
+      htmlTemplateEngine: "njk",
+      dataTemplateEngine: "njk",
+    };
+ 
+  }
+ 
